109b392e2376e6e7deaed3d6a81e1888f8bf5785
Kathlyn Lara Murussi <klmurussi@hotmail.com>
2021-05-03 14:38:44 -0300

    primeira entrega do trabalho de oo/2020/2

---
+++ gui/ProcedimentoProcurarLista.java
@@ -0,0 +1,16 @@
+package gui;
+
+
+
+import janelaFramework.JanelaFramework;
+
+class ProcedimentoProcurarLista<EstruturaDado> implements Procedimento
+{
+	protected ProcedimentoProcurarLista()
+	public void atualizar()
+	protected EstruturaDado get_estrutura_selecionada()
+	private void procedimento_botao_set_estrutura_selecionada(EstruturaDados estrutura_dados)
+	private void procedimento_botao_proxima_pagina()
+	private void procedimento_botao_pagina_anterior()
+	private void procedimento_botao_voltar()
+}
