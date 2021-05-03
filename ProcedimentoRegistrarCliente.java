28578c173db6ac007ec1fa2bfc034594137de21c
Lucas <lucas.oct8@gmail.com>
2021-05-03 13:57:53 -0300

    Adicionei Registro de Acesso, Cliente e Veiculo Adicionei JanelaFramework

---
+++ ProcedimentoRegistrarCliente.java
@@ -0,0 +1,16 @@
+package gui;
+
+
+class ProcedimentoRegistrarCliente implements Procedimento
+{
+	private String set_nome;
+	private String set_endereco;
+	private String set_celular;
+	private String set_telefone;
+	private String set_CNH;
+	
+	protected ProcedimentoRegistrarCliente ()
+	public void atualizar()
+	private void procedimento_botao_anterior()
+	private void procedimento_botao_registrar()
+}
---
+++ janelaFramework/JanelaFramework.java
@@ -0,0 +1,23 @@
+package janelaFramework;
+
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class JanelaFramework
+{
+	public JanelaFramework()
+	public void fechar()
+	public void definir_titulo()
+	public void criar_botao()
+	public void criar_texto()
+	public void set_label()
+	public void criar_campo_texto()
+	public String get_texto()
+	public void pular_linha_grafica()
+	public void mostrar()
+}
