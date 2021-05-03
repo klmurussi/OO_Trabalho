529125ccf270445c69cb06c3c3ba5ce34081a445
Lucas <lucas.oct8@gmail.com>
2021-05-03 13:57:53 -0300

    Adicionei Registro de Acesso, Cliente e Veiculo Adicionei JanelaFramework

---
+++ ProcedimentoRegistrarAcesso.java
@@ -0,0 +1,14 @@
+package gui;
+
+import dados.Veiculo;
+import janelaFramework.JanelaFramework;
+
+class ProcedimentoRegistrarAcesso implements Procedimento
+{
+	private String data_salvo;
+	private String tempo_salvo;
+	private Veiculo veiculo_salvo;
+	
+	protected ProcedimentoRegistrarAcesso()
+	public void atualizar()
+}
---
+++ ProcedimentoRegistrarVeiculo.java
@@ -0,0 +1,19 @@
+package gui;
+
+import dados.Cliente;
+
+class ProcedimentoRegistrarVeiculo implements Procedimento
+{
+	private String set_versao;
+	private String set_modelo;
+	private String set_numero;
+	private Cliente set_cliente;
+	
+	protected ProcedimentoRegistrarVeiculo()
+	public void atualizar()
+	private void procedimento_botao_registrar_cliente()
+	private void procedimento_botao_pesquisar_cliente()
+	private void procedimento_botao_mostrar_cliente()
+	private void procedimento_botao_voltar()
+	private void procedimento_botao_registrar()
+}
